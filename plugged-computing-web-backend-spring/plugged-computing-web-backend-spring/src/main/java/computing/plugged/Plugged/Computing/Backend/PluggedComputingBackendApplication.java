bca32912276182bbeec434dd98bb3c830208a704
Nataniel Cesar <cesardasilvanataniel@gmail.com>
2024-02-26 18:34:44 -0300

    initial commit

---
+++ plugged-computing-web-backend-spring/plugged-computing-web-backend-spring/src/main/java/computing/plugged/Plugged/Computing/Backend/PluggedComputingBackendApplication.java
@@ -0,0 +1,13 @@
+package computing.plugged.Plugged.Computing.Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PluggedComputingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PluggedComputingBackendApplication.class, args);
+	}
+
+}
---
+++ plugged-computing-web-backend-spring/plugged-computing-web-backend-spring/src/test/java/computing/plugged/Plugged/Computing/Backend/PluggedComputingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package computing.plugged.Plugged.Computing.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PluggedComputingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
