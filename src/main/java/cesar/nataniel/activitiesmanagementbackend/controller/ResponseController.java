e058c2a14cbffcbc9c805b0ddb2915055be332e8
Nataniel Cesar <cesardasilvanataniel@gmail.com>
2024-06-03 10:18:38 -0300

    Refactor: mudança de responsabilidade de alguns métodos de UserController -> ResponseController

--- src/main/java/cesar/nataniel/activitiesmanagementbackend/controller/ResponseController.java
+++ src/main/java/cesar/nataniel/activitiesmanagementbackend/controller/ResponseController.java
@@ -2,6 +2,7 @@ package cesar.nataniel.activitiesmanagementbackend.controller;
 
 import cesar.nataniel.activitiesmanagementbackend.model.Response;
 import cesar.nataniel.activitiesmanagementbackend.model.ResponsesStatistics;
+import cesar.nataniel.activitiesmanagementbackend.model.UserStatistics;
 import cesar.nataniel.activitiesmanagementbackend.repository.ResponseRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -113,6 +114,50 @@ public class ResponseController {
 
 
         return responsesStatistics;
+    }
+
+
+    // Endpoint to get responses of a unique user
+    @GetMapping("/getUniqueUser")
+    public List<Response> getResponsesOfUser(
+            @RequestParam String userName,
+            @RequestParam String idApp,
+            @RequestParam(name = "startDate", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date startDate,
+            @RequestParam(name = "endDate", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date endDate) {
+
+        // Filter questions by userName and idApp
+        List<Response> responses = responseRepository.findAll().stream()
+                .filter(q -> q.getUserName().equals(userName) && q.getIdApp().equals(idApp))
+                .collect(Collectors.toList());
+
+        // Checks if startDate and endDate have been provided
+        if (startDate != null && endDate != null) {
+            responses = responses.stream()
+                    .filter(q -> (q.getDateResponse().equals(startDate) || q.getDateResponse().equals(endDate)) || (q.getDateResponse().after(startDate) &&
+                            q.getDateResponse().before(endDate)))
+                    .collect(Collectors.toList());
+        }
+
+        return responses;
+    }
+
+
+    // Endpoint to get statistics of a unique user
+    @GetMapping("/getStatisticsUser")
+    public UserStatistics getStatisticsUser(
+            @RequestParam String userName,
+            @RequestParam String idApp,
+            @RequestParam(name = "startDate", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date startDate,
+            @RequestParam(name = "endDate", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date endDate) {
+
+
+        UserStatistics userStatistics = new UserStatistics();
+        List<Response> allResponses = getResponsesOfUser(userName, idApp, startDate, endDate);
+        userStatistics = userStatistics.calculateStatistics(allResponses);
+
+
+        System.out.println(userStatistics);
+        return userStatistics;
     }
 
 
--- src/main/java/cesar/nataniel/activitiesmanagementbackend/controller/UserController.java
+++ src/main/java/cesar/nataniel/activitiesmanagementbackend/controller/UserController.java
@@ -18,12 +18,11 @@ import java.util.stream.Collectors;
 public class UserController {
 
     private final UserRepository userRepository;
-    private final ResponseRepository responseRepository;
+
 
     @Autowired
-    public UserController(UserRepository userRepository, ResponseRepository responseRepository) {
+    public UserController(UserRepository userRepository) {
         this.userRepository = userRepository;
-        this.responseRepository = responseRepository;
     }
 
 
@@ -43,50 +42,4 @@ public class UserController {
         System.out.println("User created: " + userName);
 
     }
-
-
-    // Endpoint to get responses of a unique user
-    @GetMapping("/getUniqueUser")
-    public List<Response> getResponsesOfUser(
-            @RequestParam String userName,
-            @RequestParam String idApp,
-            @RequestParam(name = "startDate", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date startDate,
-            @RequestParam(name = "endDate", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date endDate) {
-
-        // Filter questions by userName and idApp
-        List<Response> responses = responseRepository.findAll().stream()
-                .filter(q -> q.getUserName().equals(userName) && q.getIdApp().equals(idApp))
-                .collect(Collectors.toList());
-
-        // Checks if startDate and endDate have been provided
-        if (startDate != null && endDate != null) {
-            responses = responses.stream()
-                    .filter(q -> (q.getDateResponse().equals(startDate) || q.getDateResponse().equals(endDate)) || (q.getDateResponse().after(startDate) &&
-                            q.getDateResponse().before(endDate)))
-                    .collect(Collectors.toList());
-        }
-
-        return responses;
-    }
-
-
-    // Endpoint to get statistics of a unique user
-    @GetMapping("/getStatisticsUser")
-    public UserStatistics getStatisticsUser(
-            @RequestParam String userName,
-            @RequestParam String idApp,
-            @RequestParam(name = "startDate", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date startDate,
-            @RequestParam(name = "endDate", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date endDate) {
-
-
-        UserStatistics userStatistics = new UserStatistics();
-        List<Response> allResponses = getResponsesOfUser(userName, idApp, startDate, endDate);
-        userStatistics = userStatistics.calculateStatistics(allResponses);
-
-
-        System.out.println(userStatistics);
-        return userStatistics;
-    }
-
-
 }
